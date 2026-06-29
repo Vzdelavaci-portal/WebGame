d7007124a487cf2c2222ab3e66ccd91f76e2aae0
Vzdelavaci-portal <info@vzdelavaci-portal.cz>
2026-06-29 13:05:43 +0200

    Add BOSS_CONFIG for game bosses

---
+++ 34BraveFortress/js/bosses.js
@@ -0,0 +1,18 @@
+const BOSS_CONFIG = Object.freeze({
+  greenValley: Object.freeze({
+    type: 'forestAncient',
+    name: 'Forest Ancient',
+    victoryTitle: 'Green Valley Secured'
+  }),
+  frozenKingdom: Object.freeze({
+    type: 'frostGiant',
+    name: 'Frost Giant',
+    victoryTitle: 'Frozen Kingdom Conquered'
+  }),
+  volcanicLands: Object.freeze({
+    type: 'fireDemon',
+    name: 'Fire Demon',
+    victoryTitle: 'Volcanic Lands Survived'
+  })
+});
+
